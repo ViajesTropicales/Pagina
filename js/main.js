771a76862804541f92dce56b0384bd079e5a5476
Andrea <andrea.cadena.2007@gmail.com>
2026-04-16 09:58:49 +0200

    cambio hamburguesa

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.nav-list');
-
+const menu = document.querySelector('#mobile-menu'); const menuLinks =
+document.querySelector('.nav-list'); 
+// Función para abrir/cerrar al dar al botón
 menu.addEventListener('click', function() {
-    // Esto añade o quita la clase 'active' cada vez que haces clic
-    menuLinks.classList.toggle('active');
-});
+menu.classList.toggle('is-active');
+menuLinks.classList.toggle('active'); }); 
+// Función EXTRA: Cerrar elmenú al hacer clic en un enlace 
+document.querySelectorAll('.nav-listli a').forEach(link => { link.addEventListener('click', () => {
+menu.classList.remove('is-active');
+menuLinks.classList.remove('active'); }); });
