5cf009016d534ee8a7c35d3db7c0eebc1322b4dd
Andrea <andrea.cadena.2007@gmail.com>
2026-04-26 19:50:04 +0200

    cambios menu hamburguesa

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.nav-list');
+const menu = document.querySelector('#btn-menu');
+const menuLinks = document.querySelector('.navbar ul');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
@@ -7,7 +7,7 @@ menu.addEventListener('click', function() {
 });
 
 
-document.querySelectorAll('.nav-list li a').forEach(link => {
+document.querySelectorAll('.navbar ul li a').forEach(link => {
     link.addEventListener('click', () => {
         menu.classList.remove('is-active');
         menuLinks.classList.remove('active');
